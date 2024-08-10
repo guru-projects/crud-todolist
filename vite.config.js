849a5c02e36ddb12ec5e562936d42f5d756da175
Guru <guru85111@gmail.com>
2024-08-10 15:06:12 +0530

    vite config change

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from "tailwindcss";
 import React from 'react';
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/guru-projects",
+  base: "/crud-todolist/",
   plugins: [react()],
   css: {
     postcss: {
