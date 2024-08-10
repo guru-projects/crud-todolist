7d13bac8eeae6f67a00dc8934394060ba52e3055
Guru <guru85111@gmail.com>
2024-08-10 18:12:21 +0530

    final design

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import React from 'react';
+import React from "react";
 function App() {
   const [task, setTask] = useState("");
   const [priority, setPriority] = useState("high");
@@ -8,19 +8,12 @@ function App() {
     JSON.parse(localStorage.getItem("ITEMS")) || []
   );
   const [filteredTasks, setFilteredTasks] = useState(tasks);
-  const [sortPriority, setSortPriority] = useState("");
   const [filteredPriority, setFilteredPriority] = useState("");
 
   useEffect(() => {
     localStorage.setItem("ITEMS", JSON.stringify(tasks));
-    handleFilterAndSort(filteredPriority, sortPriority);
-  }, [tasks, filteredPriority, sortPriority]);
-
-  const priorityOrder = {
-    high: 1,
-    mediam: 2,
-    low: 3,
-  };
+    handleFilterAndSort(filteredPriority);
+  }, [tasks, filteredPriority]);
 
   const handleChange = (e) => {
     setTask(e.target.value);
@@ -45,28 +38,15 @@ function App() {
     setTasks(newTasks);
   };
 
-  const handleSort = (e) => {
-    setSortPriority(e.target.value);
-  };
-
   const handleFilter = (e) => {
     setFilteredPriority(e.target.value);
   };
 
-  const handleFilterAndSort = (filterValue, sortValue) => {
+  const handleFilterAndSort = (filterValue) => {
     let filtered = tasks;
     if (filterValue) {
       filtered = tasks.filter((task) => task.priority === filterValue);
     }
-
-    if (sortValue) {
-      filtered.sort((a, b) => {
-        const priorityA = priorityOrder[a.priority];
-        const priorityB = priorityOrder[b.priority];
-        return priorityA - priorityB;
-      });
-    }
-
     setFilteredTasks(filtered);
   };
 
@@ -75,138 +55,123 @@ function App() {
       <div className="max-w-2xl w-screen h-96 max-h-96 bg-neutral-700 rounded overflow-auto">
         <form
           onSubmit={handleSubmit}
-          className="flex flex-col flex-wrap h-28 sticky top-0 bg-slate-400"
-        >
-          <div className="flex justify-evenly items-center">
-            <input type="text" name="search" />
+          className="flex flex-col flex-wrap h-28 sticky top-0 bg-slate-400">
+          <div className="flex m-2 rounded justify-evenly h-12 items-center relative bg-white">
+            <div className="flex absolute left-0">
+              <div className="w-4 h-4 my-4 mx-2 rounded-full bg-[#fd99af]"></div>
+              <div className="w-4 h-4 my-4 mx-1 rounded-full bg-[#34d4f4]"></div>
+              <div className="w-4 h-4 my-4 mx-1 rounded-full bg-[#fac608]"></div>
+            </div>
+            <input
+              onChange={handleChange}
+              className="text-gray-500 font-medium rounded h-12 w-full my-2 ml-24  bg-white border-none outline-none"
+              value={task}
+              type="text"
+              name="task"
+              placeholder="What is your next task?"
+            />
+            <button
+              type="submit"
+              className="bg-blue-500 px-2.5 pb-1  rounded hover:bg-blue-600 absolute right-0 mr-2 text-3xl">
+              +
+            </button>
+          </div>
+          <div className="flex items-center justify-evenly">
             <select
-              className="select after:bg-slate-300"
-              name="sort"
-              id="sort"
-              onChange={handleSort}
-              value={sortPriority}
-            >
-              <option value="">sort</option>
-              <option value="high">high</option>
-              <option value="mediam">mediam</option>
-              <option value="low">low</option>
-            </select>
-            <select
-              className="select after:bg-slate-300"
+              className="select font-medium after:bg-slate-300 bg-white text-gray-400 pl-2 outline-none rounded"
               name="filter"
               id="filter"
               onChange={handleFilter}
-              value={filteredPriority}
-            >
+              value={filteredPriority}>
               <option value="">filter</option>
               <option value="high">high</option>
               <option value="mediam">mediam</option>
               <option value="low">low</option>
             </select>
-          </div>
-          <div className="flex items-center justify-evenly">
-            <textarea
-              onChange={handleChange}
-              className="rounded w-fit"
-              value={task}
-              type="text"
-              name="task"
-              placeholder="Create a task"
-            />
             <div>
-              <label htmlFor="priority">choose priority:</label>
+              <label htmlFor="priority" className="text-white font-medium pr-2">
+                choose priority:
+              </label>
               <select
+                className="font-medium bg-white text-gray-400 pl-2 outline-none rounded"
                 onChange={handlePriority}
                 value={priority}
                 name="dropdown"
-                id="priority"
-              >
+                id="priority">
                 <option value="high">high</option>
                 <option value="mediam">mediam</option>
                 <option value="low">low</option>
               </select>
             </div>
-            <button
-              type="submit"
-              className="bg-blue-500 px-3 py-1 rounded hover:bg-blue-600"
-            >
-              submit
-            </button>
           </div>
         </form>
+
         <div className="p-3 flex items-center justify-center content-center flex-col-reverse">
           {filteredTasks.map((taskItem, index) => (
-            <div
-              key={index}
-              className={`w-full rounded mb-3 flex justify-between ${
-                taskItem.priority === "high"
-                  ? "bg-red-300"
-                  : taskItem.priority === "mediam"
-                  ? "bg-yellow-300"
-                  : "bg-green-300"
-              }`}
-            >
-              <p className="text-center min-h-10 p-3">
-                {taskItem.task} - {taskItem.priority}
-              </p>
-              <button
-                className="bg-red-200 px-3 py-1 rounded hover:bg-red-600 flex gap-2 items-center"
-                onClick={() => handleDelete(index)}
-              >
-                Delete
-                <svg
-                  className="w-8"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                  stroke="#ff8080"
-                >
-                  <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
-                  <g
-                    id="SVGRepo_tracerCarrier"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  ></g>
-                  <g id="SVGRepo_iconCarrier">
-                    {" "}
-                    <path
-                      d="M10 12V17"
-                      stroke="#ff7070"
-                      strokeWidth="2"
+            <div className={"flex w-full rounded mb-3 bg-white"}>
+              <div
+                key={index}
+                className={`w-4 h-4 m-4 rounded-full ${
+                  taskItem.priority === "high"
+                    ? "bg-[#fd99af]"
+                    : taskItem.priority === "mediam"
+                    ? "bg-[#34d4f4]"
+                    : "bg-[#fac608]"
+                }`}></div>
+              <div className=" w-full flex justify-between">
+                <p className="text-center min-h-10 p-3 text-gray-500">
+                  {taskItem.task}
+                </p>
+                <button
+                  className=" px-3 py-1 rounded hover:bg-red-200 flex gap-2 items-center"
+                  onClick={() => handleDelete(index)}>
+                  <svg
+                    className="w-8"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    stroke="#ff8080">
+                    <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                    <g
+                      id="SVGRepo_tracerCarrier"
                       strokeLinecap="round"
-                      strokeLinejoin="round"
-                    ></path>{" "}
-                    <path
-                      d="M14 12V17"
-                      stroke="#ff7070"
-                      strokeWidth="2"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    ></path>{" "}
-                    <path
-                      d="M4 7H20"
-                      stroke="#ff7070"
-                      strokeWidth="2"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    ></path>{" "}
-                    <path
-                      d="M6 10V18C6 19.6569 7.34315 21 9 21H15C16.6569 21 18 19.6569 18 18V10"
-                      stroke="#ff7070"
-                      strokeWidth="2"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    ></path>{" "}
-                    <path
-                      d="M9 5C9 3.89543 9.89543 3 11 3H13C14.1046 3 15 3.89543 15 5V7H9V5Z"
-                      stroke="#ff7070"
-                      strokeWidth="2"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    ></path>{" "}
-                  </g>
-                </svg>
-              </button>
+                      strokeLinejoin="round"></g>
+                    <g id="SVGRepo_iconCarrier">
+                      {" "}
+                      <path
+                        d="M10 12V17"
+                        stroke="#ff7070"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"></path>{" "}
+                      <path
+                        d="M14 12V17"
+                        stroke="#ff7070"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"></path>{" "}
+                      <path
+                        d="M4 7H20"
+                        stroke="#ff7070"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"></path>{" "}
+                      <path
+                        d="M6 10V18C6 19.6569 7.34315 21 9 21H15C16.6569 21 18 19.6569 18 18V10"
+                        stroke="#ff7070"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"></path>{" "}
+                      <path
+                        d="M9 5C9 3.89543 9.89543 3 11 3H13C14.1046 3 15 3.89543 15 5V7H9V5Z"
+                        stroke="#ff7070"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"></path>{" "}
+                    </g>
+                  </svg>
+                </button>
+              </div>
             </div>
           ))}
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 export default {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
+    // colors: {
+    //   red: '#ED6A61',
+    //   yellow: '#ED6A61',
+    //   green: '#ED6A61',
+    // },
     extend: {},
   },
   plugins: [],
